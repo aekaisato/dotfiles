4f1f4eb23181858e0a1965fe2854e81c4521a90a
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-03-08 14:23:57 -0500

    disable llm garbage

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("browser.ml.chat.enabled", false);
